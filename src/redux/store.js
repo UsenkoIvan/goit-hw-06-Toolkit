9956e714e0def3f58ea9a3991549d392f11ba8f9
Ivan Usenko <ivanja1994@gmail.com>
2020-06-15 21:49:34 +0300

    add persist-redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "../redux/reducers";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "contacts",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
